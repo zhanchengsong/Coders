0d7fb57df49b200fea48350617548ad6e1c73fc5
zhanchengsong <zhanchengdev@gmail.com>
2020-02-23 15:26:25 -0500

    Created Spring Boot Project

---
+++ coder-backend/src/main/java/dev/newgrad/coder/coderbackend/CoderBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.newgrad.coder.coderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderBackendApplication.class, args);
+    }
+
+}
---
+++ coder-backend/src/test/java/dev/newgrad/coder/coderbackend/CoderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.newgrad.coder.coderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
